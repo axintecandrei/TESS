5614d11def21551522cc13eb9dbbdecca513ff97
Andrei Axinte <axintecandrei@gmail.com>
2018-03-24 19:13:07 +0200

    remove ignored files

--- TESS_Eclipse/Release/TESS_main/tess_math.c.d
+++
@@ -1,8 +0,0 @@
-TESS_main\tess_math.c.o: \
- D:\Things\EPAE\Microcontrolere\Arduino\Arduino_sketches\TESS\TESS_Eclipse\TESS_main\tess_math.c \
- D:\Things\EPAE\Microcontrolere\Arduino\Arduino_sketches\TESS\TESS_Eclipse\TESS_main\tess_math.h \
- D:\Things\EPAE\Microcontrolere\Arduino\Arduino_sketches\TESS\TESS_Eclipse\TESS_main\tess_sal_types.h
-
-D:\Things\EPAE\Microcontrolere\Arduino\Arduino_sketches\TESS\TESS_Eclipse\TESS_main\tess_math.h:
-
-D:\Things\EPAE\Microcontrolere\Arduino\Arduino_sketches\TESS\TESS_Eclipse\TESS_main\tess_sal_types.h:
