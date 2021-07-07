f2343a098e9860fbf5ff91685b7fd60f7757880c
Andrei Axinte <axintecandrei@gmail.com>
2021-07-07 21:07:48 +0300

    Clean build outpus

--- ProjectDevelopment/Debug/TESS/Actuators/ActStMachine/tess_act_statemachine.d
+++
@@ -1,5 +0,0 @@
-TESS/Actuators/ActStMachine/tess_act_statemachine.o: \
- ../TESS/Actuators/ActStMachine/tess_act_statemachine.c \
- ../TESS/Actuators/ActStMachine/tess_act_statemachine.h
-
-../TESS/Actuators/ActStMachine/tess_act_statemachine.h:
